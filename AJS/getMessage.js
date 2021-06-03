9e04439b34d5d957e18c6aafb97d67ee1a2544f7
UpgradeAll rule <sdfnkfs@126.com>
2021-06-04 06:26:39 +0800

    Update getMessage.js

--- AJS/getMessage.js
+++ AJS/getMessage.js
@@ -1,4 +1,4 @@
-var r = http.get("http://hq.sinajs.cn/list=sh000001,sz399001,sz002594,sz300122,sz300750,sz300760,sz000858,sh603345,sz002027,sz002352,sh603288,sz300274", {
+var r = http.get("http://hq.sinajs.cn/list=sh000001,sz399001,sz002594,sz300122,sz300750,sz300760,sz300782,sh603345,sh600588,sz002352,sh603288,sz300274", {
     headers: {
         'Accept-Language': 'zh-cn,zh;q=0.5',
         'User-Agent': 'Mozilla/5.0(Macintosh;IntelMacOSX10_7_0)AppleWebKit/535.11(KHTML,likeGecko)Chrome/17.0.963.56Safari/535.11'
